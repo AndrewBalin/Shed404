1ff113fd686a0c328ce6bec2faa43dc9baa7ceca
AndrewBalin <i@andrew-balin.ru>
2025-05-01 15:50:20 +0500

    [Interactive Logic] - Player controller, camera follow and interacive use cases

---
+++ Assets/Activity/Scripts/Interactive/Interactable.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+namespace Interactive
+{
+    [RequireComponent(typeof(Collider))]
+    public class Interactable : MonoBehaviour
+    {
+        // Дистанция взаимодействия
+        [Header("Interactive Settings")]
+        public float interactionDistance = 3f;
+        public InteractiveUseCase useCase;
+        
+        void Start()
+        {
+            this.GetComponent<CustomPassVolume>().customPasses[0].enabled = false;
+        }
+        
+        public void OnHover()
+        {
+            this.GetComponent<CustomPassVolume>().customPasses[0].enabled = true;
+        }
+        
+        public void OnUnhover()
+        {
+            this.GetComponent<CustomPassVolume>().customPasses[0].enabled = false;
+        }
+        
+        public void PerformAction(string action, NavMeshAgent agent)
+        {
+            Debug.Log($"Action {action} performed on {gameObject.name}");
+            useCase.Interact(this, agent, action);
+        }
+    }
+}
---
+++ Assets/Activity/Scripts/Interactive/InteractiveUseCase.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace Interactive
+{
+    public class InteractiveUseCase : MonoBehaviour
+    {
+        // Класс от которого будет наследоваться любая
+        // интерактивность в игре с управлением Point&Click
+        public void Interact(Interactable interactable, NavMeshAgent agent, string action)
+        /*
+         * :interactable: объект с которым взаимодействуем
+         * :agent: игрок
+         */
+        {
+            // Здесь вы можете добавить логику взаимодействия с объектом
+            // игроком, или чем-то другим
+        }
+    }
+}
